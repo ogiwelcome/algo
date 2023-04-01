68fd4a3f8941239ac66dd4750c1369a9581398de
ogino masahiro <oginomasahiro@oginononotopasokon.local>
2023-04-01 21:45:12 +0900

    add

---
+++ atcoder/cs/abc260/a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+  static void Main()
+  {
+    var s = Console.ReadLine();
+    var dis = s?.ToCharArray().GroupBy(x => x).FirstOrDefault(x => x.Count() == 1);
+    Console.WriteLine(dis == default ? "-1" : dis.First().ToString());
+  }
+}
---
+++ atcoder/cs/abc260/b/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace abc260_b
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+      var n = arr[0];
+      var x = arr[1];
+      var y = arr[2];
+      var z = arr[3];
+      var a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+      var b = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+      var r = new List<int>();
+      var q = Enumerable.Range(0, n).Select(i => (id: i + 1, a: a[i], b: b[i])).ToArray();
+
+      q = q.OrderBy(p => -p.a).ThenBy(p => p.id).ToArray();
+      r.AddRange(q[..x].Select(p => p.id));
+      q = q[x..];
+
+      q = q.OrderBy(p => -p.b).ThenBy(p => p.id).ToArray();
+      r.AddRange(q[..y].Select(p => p.id));
+      q = q[y..];
+
+      q = q.OrderBy(p => -p.a - p.b).ThenBy(p => p.id).ToArray();
+      r.AddRange(q[..z].Select(p => p.id));
+      q = q[z..];
+
+      r.Sort();
+      Console.WriteLine(string.Join("\n", r));
+    }
+  }
+}
